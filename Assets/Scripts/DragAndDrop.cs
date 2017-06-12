3e30bf8f2e05bdd7b2bd62bfdff0419796ae8cb8
leigh <lagebergman@gmail.com>
2017-06-12 19:27:40 +0200

    Add outline for drag and drop script

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour {
+
+	private bool mouseDown;
+
+	void OnMouseDown() {
+		mouseDown = true;
+	}
+
+	void OnMouseDrag() {
+		if (mouseDown) {
+			//TODO: Rotate limb
+		}
+	}
+
+	void OnMouseUp() {
+		mouseDown = false;
+	}
+}
