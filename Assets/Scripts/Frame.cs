fcb59b104df53e5ed69d35135ddaff84611f6f9a
leigh <lagebergman@gmail.com>
2017-06-12 15:32:44 +0200

    Add Frame class

---
+++ Assets/Scripts/Frame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Frame {
+
+	private Dictionary<string,float> bodyPartRotations;
+	private float defaultRotation;
+
+	public Frame(){
+		bodyPartRotations = new Dictionary<string, float> ();
+	}
+
+	public void addBodyPartRoation(string bodyPart, float rotation) {
+		if (bodyPartRotations.ContainsKey (bodyPart)) {
+			bodyPartRotations.Remove (bodyPart);
+		}
+		bodyPartRotations.Add (bodyPart, rotation);
+	}
+
+	public float getRotation(string bodyPart){
+		return bodyPartRotations.TryGetValue (bodyPart, defaultRotation);
+	}
+
+}
