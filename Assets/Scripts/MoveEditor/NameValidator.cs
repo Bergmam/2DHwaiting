596cbad94b49499488c27122546549ed492c6f9e
leigh <lagebergman@gmail.com>
2017-07-09 16:22:27 +0200

    Hide name already in use text is name is valid

---
+++ Assets/Scripts/MoveEditor/NameValidator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameValidator : MonoBehaviour {
+
+	private GameObject alreadyUsedText;
+	private InputField inputField;
+	private string newName;
+	private bool nameExists;
+
+	// Use this for initialization
+	void Start () {
+		alreadyUsedText = transform.FindChild ("AlreadyUsedText").gameObject;
+		alreadyUsedText.SetActive (false);
+		inputField = transform.FindChild ("NameInputField").gameObject.GetComponent<InputField> ();
+	}
+
+	public void ValidateName()
+	{
+		name = inputField.text;
+		nameExists = AvailableMoves.ContainsName (name);
+		alreadyUsedText.SetActive (nameExists);
+	}
+
+	public bool IsNameValid()
+	{
+		return !nameExists;
+	}
+}
