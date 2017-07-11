624ace161c1802e53905e1313ba1ac665ce3aa38
Anton Bergman <antonbergman_@hotmail.com>
2017-07-11 09:19:39 +0200

    Scene Changes for fixing knockback collisions on both characters

--- Assets/Scripts/CharacterControl/BodyPartTriggerHandler.cs
+++ Assets/Scripts/CharacterControl/BodyPartTriggerHandler.cs
@@ -26,35 +26,35 @@ public class BodyPartTriggerHandler : MonoBehaviour {
     {
         GameObject otherGameObject = other.gameObject;
         Move move = inputController.GetCurretlyPlayedMove();
-        
         // If the collided object is not part of the same character
-        if (otherGameObject.transform.root.name != transform.root.name &&  move != null)
+        if (move != null)
         {
-            print("other: " + otherGameObject.name);
-            print("me: " + transform.name);
-            print("self: " + transform.root.name);
-            InputController otherInputController = otherGameObject.GetComponent<InputController>();
-            Character otherCharacter = otherInputController.GetCharacter();
-            
-            // Do damage to the other character
-            otherCharacter.ApplyMoveTo(move);
-
-            // TODO: Remove once healthbars are implemented
-            print("Move '" + inputController.GetCurretlyPlayedMove().GetName() + "' applied to character " + otherCharacter.GetNbr());
-            print("Character " + otherCharacter.GetNbr() + " now has " + otherCharacter.GetHealth() + " health");
-
-            Vector3 thisPosition = transform.root.transform.position;
-            Vector3 otherPosition = otherGameObject.transform.position;
-            Rigidbody2D otherBody = otherGameObject.GetComponent<Rigidbody2D>();
-            
-            if (otherPosition.x > thisPosition.x)
+            if (otherGameObject.transform.root.name != transform.root.name)
             {
-                otherBody.AddForce(Vector2.right * 250);
-            }
-            else
-            {
-                otherBody.AddForce(Vector2.left * 250);
+                InputController otherInputController = otherGameObject.GetComponent<InputController>();
+                Character otherCharacter = otherInputController.GetCharacter();
+
+                // Do damage to the other character
+                otherCharacter.ApplyMoveTo(move);
+
+                // TODO: Remove once healthbars are implemented
+                print("Move '" + inputController.GetCurretlyPlayedMove().GetName() + "' applied to character " + otherCharacter.GetNbr());
+                print("Character " + otherCharacter.GetNbr() + " now has " + otherCharacter.GetHealth() + " health");
+
+                Vector3 thisPosition = transform.root.transform.position;
+                Vector3 otherPosition = otherGameObject.transform.position;
+                Rigidbody2D otherBody = otherGameObject.GetComponent<Rigidbody2D>();
+
+                if (otherPosition.x > thisPosition.x)
+                {
+                    otherBody.AddForce(Vector2.right * 250);
+                }
+                else
+                {
+                    otherBody.AddForce(Vector2.left * 250);
+                }
             }
         }
+        
     }
 }
