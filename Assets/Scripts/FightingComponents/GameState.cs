c840762ae66caedc781faa5902cfca9df3613d04
leigh <lagebergman@gmail.com>
2017-07-12 09:47:01 +0200

    Implement methods for pausing an unpausing the game. Paus game is called when a player dies.

--- Assets/Scripts/FightingComponents/GameState.cs
+++ Assets/Scripts/FightingComponents/GameState.cs
@@ -9,11 +9,9 @@ public class GameState : MonoBehaviour
 {
 
 	private ProgressBarBehaviour[] healthBars;
-	private List<Character> characters;
 
 	void Start ()
 	{
-		characters = StaticCharacterHolder.characters;
 		healthBars = new ProgressBarBehaviour[StaticCharacterHolder.characters.Count];
 		ProgressBarBehaviour character1HealthBar = GameObject.Find ("Character1HealthBar").GetComponent<ProgressBarBehaviour> ();
 		healthBars [0] = character1HealthBar;
@@ -33,5 +31,49 @@ public class GameState : MonoBehaviour
 		float percentage = health / maxHealth;
 		int characterIndex = character.GetNbr() - 1;
 		healthBars [characterIndex].UpdateFill (percentage);
+
+		//Freeze game if a player dies.
+		if (health <= 0)
+		{
+			PauseGame ();
+		}
+	}
+
+	/// <summary>
+	/// Pauses the game by disabling animation and control on both characters.
+	/// </summary>
+	public void PauseGame()
+	{
+		SetCharacterEnabled(GameObject.Find ("Character 1"), false);
+		SetCharacterEnabled(GameObject.Find ("Character 2"), false);
+	}
+
+	/// <summary>
+	/// Uns the pause game.
+	/// Unpauses the game by enabling animation and control on both characters.
+	/// </summary>
+	public void UnPauseGame()
+	{
+		SetCharacterEnabled(GameObject.Find ("Character 1"), true);
+		SetCharacterEnabled(GameObject.Find ("Character 2"), true);
+	}
+
+	/// <summary>
+	/// Disable or enable the specified character according to the bool parameter.
+	/// The enabled bool should be true for enabling and false for disabling.
+	/// </summary>
+	/// <param name="characterObject">Character object.</param>
+	/// <param name="enabled">If set to <c>true</c> enabled.</param>
+	private void SetCharacterEnabled(GameObject characterObject, bool enabled)
+	{
+		characterObject.GetComponent<InputController> ().enabled = enabled; //Change state of character controls.
+		//Change state of collision detection.
+		foreach (BodyPartTriggerHandler collisionHandler in characterObject.GetComponentsInChildren<BodyPartTriggerHandler>()) {
+			collisionHandler.enabled = enabled;
+		}
+		//Change state of animation.
+		foreach (Animator animator in characterObject.GetComponentsInChildren<Animator>()) {
+			animator.enabled = enabled;
+		}
 	}
 }
