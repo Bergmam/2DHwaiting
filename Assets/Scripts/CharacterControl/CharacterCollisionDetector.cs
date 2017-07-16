909cbb2b8966d1a40e96562e5c7482382f9e5505
leigh <lagebergman@gmail.com>
2017-07-17 00:18:59 +0800

    Handle collisions in character root object and not in damage dealer

---
+++ Assets/Scripts/CharacterControl/CharacterCollisionDetector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCollisionDetector : MonoBehaviour
+{
+	private Character thisCharacter;
+	private GameState gameState;
+
+	void Start ()
+	{
+		thisCharacter = transform.root.gameObject.GetComponent<InputController> ().GetCharacter ();
+		this.gameState = GameObject.Find ("Handler").GetComponent<GameState> ();
+	}
+	
+	void OnTriggerEnter2D (Collider2D otherCollider)
+	{
+		GameObject otherCharacterObject = otherCollider.transform.root.gameObject;
+		InputController otherInputController = otherCharacterObject.GetComponent<InputController> (); //Get attacking character's InputController
+		if (otherInputController == null)
+		{
+			return; //If colliding object's root does not have an InputController, it is not a character.
+		}
+		Character otherCharacter = otherInputController.GetCharacter ();
+		Move move = otherInputController.GetCurretlyPlayedMove ();
+		if (otherCharacter == null || move == null || otherCharacter.Equals (thisCharacter))
+		{
+			return; //Make sure other character object has all necessary info.
+		}
+		thisCharacter.ApplyMoveTo (move); //Apply damage in model.
+		gameState.UpdateCharacterHealth (thisCharacter); //Update health bars and check winner.
+		otherCollider.enabled = false;
+
+		//TODO: Implement knockback.
+	}
+}
