42ee03dfa33ed2b9416b38e9e0307d973e8f9288
leigh <lagebergman@gmail.com>
2017-10-31 19:38:13 +0100

    Add comments in FightCharacterSpawner

--- Assets/Scripts/Prefabs/FightCharacterSpawner.cs
+++ Assets/Scripts/Prefabs/FightCharacterSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FightCharacterSpawner : MonoBehaviour
 {
 
-	public float x1, x2, y1, y2;
+	public float x1, x2, y1, y2; // (x1,y1) is position of character 1. (x2,y2) is position of character 2. 
 
 	void Start()
 	{
@@ -20,10 +20,11 @@ public class FightCharacterSpawner : MonoBehaviour
 		GameObject character2 = Instantiate(Resources.Load("Prefabs/Character", typeof(GameObject))) as GameObject;
 
 		character1.transform.localScale = new Vector3 (character1.transform.localScale.x, character1.transform.localScale.y, character1.transform.localScale.z);
+		// - to change direction of character 2.
 		character2.transform.localScale = new Vector3 (-character2.transform.localScale.x, character2.transform.localScale.y, character2.transform.localScale.z);
 
-		RemoveUnnecessaryComponents (character1, 1);
-		RemoveUnnecessaryComponents (character2, 2);
+		AndAndRemoveComponents (character1, 1);
+		AndAndRemoveComponents (character2, 2);
 
 		character2.GetComponent<InputController>().horizontalAxis = "Horizontal2";
 		character2.GetComponent<InputController>().verticalAxis = "Vertical2";
@@ -32,7 +33,7 @@ public class FightCharacterSpawner : MonoBehaviour
 		character2.transform.position = new Vector3(x2, y2, 0);
 	}
 
-	private void RemoveUnnecessaryComponents(GameObject characterObject, int index)
+	private void AndAndRemoveComponents(GameObject characterObject, int index)
 	{
 		Transform characterTransform = characterObject.transform;
 		characterObject.transform.name = "Character " + index;
@@ -40,9 +41,12 @@ public class FightCharacterSpawner : MonoBehaviour
         characterObject.GetComponent<Animator>().enabled = true;
         characterObject.GetComponent<InputController>().enabled = true;
         characterObject.GetComponent<Rigidbody2D>().simulated = true;
+
+		//Turn on hitbox colliders (character = legs & torso = upper body)
         characterObject.GetComponent<BoxCollider2D>().enabled = true;
         Transform torsoTransform = characterTransform.Find("Torso");
         torsoTransform.GetComponent<BoxCollider2D>().enabled = true;
+
         characterObject.GetComponent<CharacterCollisionDetector>().enabled = true;
         characterObject.GetComponent<DamageTriggerDetector>().enabled = true;
 
@@ -51,21 +55,13 @@ public class FightCharacterSpawner : MonoBehaviour
         
         foreach (Rigidbody2D body in characterObject.GetComponentsInChildren<Rigidbody2D>())
         {
-            body.simulated = true;
+			body.simulated = true; //Turn on rigidbody simlutaion (No "enabled" bool in rigidbody).
         }
 
         foreach (DamageTriggerDetector damageDetector in characterObject.GetComponentsInChildren<DamageTriggerDetector>())
 		{
 			damageDetector.enabled = true;
-            damageDetector.characterIndex = index;
+            damageDetector.characterIndex = index; //Make sure no damage is dealt on collision with self.
         }
-
-        /*foreach (GameObject child in UnityUtils.RecursiveContains(characterTransform, "DragPoint"))
-		{
-			if (child.name.Contains("DragPoint"))
-			{
-				Destroy(child);
-			}
-        }*/
     }
 }
