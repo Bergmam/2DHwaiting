054adf9b24abb03aa71ad2389c01e02ddb82b0be
leigh <lagebergman@gmail.com>
2017-12-30 09:41:21 +0100

    Fix character shadows at ground level

---
+++ Assets/Scripts/FightingComponents/ShadowScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowScript : MonoBehaviour {
+
+	private float height;
+
+	void Update ()
+	{
+		if (Mathf.Abs (transform.position.y - height) > 0.01f) //Corect shadow height if it is too far from ground.
+		{
+			float x = transform.position.x;
+			float z = transform.position.z;
+			Vector3 newPosition = new Vector3 (x, height, z);
+			transform.position = newPosition;
+		}
+	}
+
+	public void SetHeight(float height){
+		this.height = height;
+	}
+}
